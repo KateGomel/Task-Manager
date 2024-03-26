2b75a5a9198059b9127684a4ce0b6e09956a9ecd
Katsiaryna <ykatuay@gmail.com>
2024-03-26 20:00:28 +0300

    ad hbs plug

--- main.js
+++ main.js
@@ -1,23 +1,4 @@
 import "lucide-static/font/Lucide.css";
 import "./style.css";
-import template from "./counter.hbs";
-import { compile } from "handlebars";
 
 const root = document.getElementById("app");
-
-let counter = 0;
-
-const hbs = compile(template());
-root.innerHTML = hbs({ counter });
-
-const plus = document.querySelector(".plus");
-const content = document.querySelector(".content");
-const minus = document.querySelector(".minus");
-
-plus.addEventListener("click", () => {
-  content.textContent = counter += 1;
-});
-
-minus.addEventListener("click", () => {
-  content.textContent = counter -= 1;
-});
