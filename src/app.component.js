66d68feb44c39b3402dd911d71c088a4b1e0ada8
Katsiaryna <ykatuay@gmail.com>
2024-03-28 19:40:43 +0300

    add page

--- src/app.component.js
+++ src/app.component.js
@@ -1,16 +1,22 @@
 import { Component } from "./core/Component";
 import template from "./app.template.hbs";
+import { ROUTES } from "./constants/routes";
 
-import './core/Router';
+import "./core/Router";
 
-import './pages/home/home.component'
-import './pages/not-found/not-found.component'
-import './pages/board/board.component'
+import "./pages/home/home.component";
+import "./pages/not-found/not-found.component";
+import "./pages/board/board.component";
+import "./pages/dashboard/dashboard.component";
+import "./pages/sign-in/sing-in.component";
+import "./pages/sign-up/sing-up.component";
 
 export class App extends Component {
   constructor() {
     super();
-    this.template = template();
+    this.template = template({
+      routes: ROUTES,
+    });
     this.state = {};
   }
 }
---
+++ src/pages/dashboard/dashboard.component.js
@@ -0,0 +1,11 @@
+import { Component } from "../../core/Component";
+import template from "./dashboard.template.hbs";
+
+export class Dashboard extends Component {
+  constructor() {
+    super();
+    this.template = template();
+  }
+}
+
+customElements.define("dashboard-page", Dashboard);
