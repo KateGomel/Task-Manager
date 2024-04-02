d614efba6c18320c128706d93f71e1cfd945986a
Katsiaryna <ykatuay@gmail.com>
2024-04-02 17:26:06 +0300

    add sing-up

--- src/pages/sign-up/sing-up.component.js
+++ src/pages/sign-up/sing-up.component.js
@@ -1,10 +1,15 @@
 import { Component } from "../../core/Component";
 import template from "./sing-up.template.hbs";
+import { ROUTES } from "../../constants/routes";
+import "../../components/input/input.component";
+import "../../components/button/button.component";
 
 export class SingUp extends Component {
   constructor() {
     super();
-    this.template = template();
+    this.template = template({
+      routes: ROUTES,
+    });
   }
 }
 
