61cf756284b38284db75b9413a5078bb70fe4a43
KateGomel <ykatuay@gmail.com>
2024-04-03 12:40:46 +0300

    ok

--- src/pages/sign-up/sign-up.component.js
+++ src/pages/sign-up/sign-up.component.js
@@ -1,22 +1,57 @@
 import { Component } from "../../core/Component";
 import template from "./sign-up.template.hbs";
 import { ROUTES } from "../../constants/routes";
-
-import "../../components/input/input.component";
-import "../../components/button/button.component";
-import "../../components/loader/loader.component";
+import { extractFormData } from "../../utils/extractFormData";
+import { authService } from "../../services/Auth";
+import { useToastNotification } from "../../hooks/useToastNotification";
+import { TOAST_TYPE } from "../../constants/toast";
+import { useNavigate } from "../../hooks/useNavigate";
 
 export class SignUp extends Component {
   constructor() {
     super();
 
+    this.template = template({ routes: ROUTES });
     this.state = {
       isLoading: false,
     };
+  }
 
-    this.template = template({
-      routes: ROUTES,
+  toggleIsLoading = () => {
+    this.setState({
+      ...this.state,
+      isLoading: !this.state.isLoading,
     });
+  };
+
+  registerUser = (evt) => {
+    evt.preventDefault();
+    const formData = extractFormData(evt.target);
+    this.toggleIsLoading();
+    authService
+      .signUp(formData.email, formData.password)
+      .then((data) => {
+        console.log(data);
+        useToastNotification({
+          message: "Success!!!",
+          type: TOAST_TYPE.success,
+        });
+        useNavigate(ROUTES.dashboard);
+      })
+      .catch((error) => {
+        useToastNotification({ message: error.message });
+      })
+      .finally(() => {
+        this.toggleIsLoading();
+      });
+  };
+
+  componentDidMount() {
+    this.addEventListener("submit", this.registerUser);
+  }
+
+  componentWillUnmount() {
+    this.removeEventListener("submit", this.registerUser);
   }
 }
 
