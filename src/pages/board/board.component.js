b9e539bd72011dcce16795b3fa390b991aa5df8b
Katsiaryna <ykatuay@gmail.com>
2024-04-11 20:35:14 +0300

    ggg

--- src/pages/board/board.component.js
+++ src/pages/board/board.component.js
@@ -1,11 +1,27 @@
 import { Component } from "../../core/Component";
-import template from './board.template.hbs';
+import template from "./board.template.hbs";
+import { useUserStore } from "../../hooks/useUserStore";
 
 export class BoardPage extends Component {
-    constructor() {
-        super();
-        this.template = template();
-    }
+  constructor() {
+    super();
+    this.state = {
+      boardId: null,
+      user: null,
+      isLoading: false,
+    };
+
+    this.template = template();
+  }
+
+  initialization() {
+    const { getUser } = useUserStore();
+    this.setState({
+      ...this.state,
+      boardId: this.getAttribute("id"),
+      user: getUser(),
+    });
+  }
 }
 
-customElements.define('board-page', BoardPage)
+customElements.define("board-page", BoardPage);
